3dee6885421029aa29c3688241d9d50a5ae9697d
dgana <dganashok@gmail.com>
2024-03-30 21:28:24 +0200

    Added donwload (photo, video, voice)

--- telegram_save_message.py
+++ telegram_save_message.py
@@ -45,22 +45,66 @@ class telegram:
             self.app.run()  
 
     def messages_file(self):
-        def save_file(message):
+        async def save_file(message):
             check_or_create_directory(str(message.chat.id))
             check_or_create_file('text.json', str(message).replace('\n', ''))
-            print(message.date, message.chat.id, message.chat.first_name)
-            os.chdir('../')
+
         
         if self.acknowledge or self.consider:
             check_or_create_directory('users')
             @self.app.on_message()
             async def serch(client, message):
                 if message.chat.id in self.consider:
-                    save_file(message)
+                    save_file( message)
+
+
+                    print(message)
+                    if message.photo:
+                        file = await self.app.download_media(message.photo.file_id)
+                        file_name = str(message.photo.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.png', 'w') as f:
+                            f.write(file)
+
+                    elif message.video:
+                        file = await self.app.download_media(message.video.file_id)
+                        file_name = str(message.video.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.mp4', 'w') as f:
+                            f.write(file)
+
+                    elif message.voice:
+                        file = await self.app.download_media(message.voice.file_id)
+                        file_name = str(message.voice.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.mp3', 'w') as f:
+                            f.write(file)
+
+                    print(message.date, message.chat.id, message.chat.first_name)
+                    os.chdir('../')
                 
                 if message.chat.id not in self.acknowledge:
                     save_file(message)
-               
+
+                    print(message)
+                    if message.photo:
+                        file = await self.app.download_media(message.photo.file_id)
+                        file_name = str(message.photo.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.png', 'w') as f:
+                            f.write(file)
+
+                    elif message.video:
+                        file = await self.app.download_media(message.video.file_id)
+                        file_name = str(message.video.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.mp4', 'w') as f:
+                            f.write(file)
+
+                    elif message.voice:
+                        file = await self.app.download_media(message.voice.file_id)
+                        file_name = str(message.voice.date).replace(':', '-')
+                        with open(f'{message.chat.id}/{file_name}.mp3', 'w') as f:
+                            f.write(file)
+
+                    print(message.date, message.chat.id, message.chat.first_name)
+                    os.chdir('../')
+                    
             self.app.run()
 
     
